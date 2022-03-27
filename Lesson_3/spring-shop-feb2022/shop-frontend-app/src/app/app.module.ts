fbe8a50bbc45bc7bc15819f6386775b757acb338
svnat <svnat92@gmail.com>
2022-03-27 23:53:26 +0300

    Lesson3
    Выполнил ДЗ к уроку №3

---
+++ Lesson_3/spring-shop-feb2022/shop-frontend-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductGalleryPageComponent } from './page/product-gallery-page/product-gallery-page.component';
+import { ProductInfoPageComponent } from './page/product-info-page/product-info-page.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ProductFilterComponent } from './component/product-filter/product-filter.component';
+import { ProductGalleryComponent } from './component/product-gallery/product-gallery.component';
+import { PaginationComponent } from './component/pagination/pagination.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductGalleryPageComponent,
+    ProductInfoPageComponent,
+    NavBarComponent,
+    ProductFilterComponent,
+    ProductGalleryComponent,
+    PaginationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
